7cdd5f972a3d9292802e7d577bdf496770994dec
colourlife-WL <1936129524@qq.com>
2018-09-20 10:19:42 +0800

    提交一个demo关于spring-boot

--- demo/src/main/java/com/example/web/HelloWorldController.java
+++ demo/src/main/java/com/example/web/HelloWorldController.java
@@ -1,14 +1,16 @@
 package com.example.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String hello(){
-        return "Hello world !";
+    @RequestMapping(value = "/hel",produces="application/json;charset=UTF-8")
+    public String hello() throws Exception{
+       // return "你好";
+        throw new Exception("发生错误123");
     }
-
 }
+
